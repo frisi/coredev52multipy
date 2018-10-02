17c85bf5a65a4a77c5fb4242efb1c7da1df55648
Harald Friessnegger <harald@webmeisterei.com>
2018-10-02 11:19:33 +0200

    fix bootstrap script
    * fix wrong paths to requirements.txt
    * ubuntu virtualenv issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,14 @@ git clone -b 5.2 git@github.com:plone/buildout.coredev.git
 # for py 2.7
 cd py2
 virtualenv -p /usr/bin/python2.7 --clear .
-./bin/pip install -r requirements.txt
+./bin/pip install -r ../buildout.coredev/requirements.txt
 ./bin/buildout -N
 
 # for py3.6
 cd ../py3
-python3.6 -m venv .
-./bin/pip install -r requirements.txt
+#this results in an error https://askubuntu.com/questions/958303/unable-to-create-virtual-environment-with-python-3-6 on my ubuntu installation
+#python3.7 -m venv .
+#old style virtualenv
+virtualenv --python=python3.6 --clear .
+./bin/pip install -r ../buildout.coredev/requirements.txt
 ./bin/buildout -N
